1f5670c53bf3ffd608a4eb84eab0fd051fca395c
Rubens Koji Miura <rubenskm@yahoo.com.br>
2013-07-09 19:42:28 +0000

    Modified

--- web.js
+++ web.js
@@ -3,10 +3,12 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+    var file = fs.readFileSync("index.html", "UTF-8");
+    var buffer = file.toString('utf8',0, file.length);
+    buffer.toString('utf-8');
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+  console.log("Listening on "+ port);
 });
